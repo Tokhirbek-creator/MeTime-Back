62b8b5d2627d11c87e33cf5008f0e037f5aaf05f
Timur Minzulin <timurminzulin@MacBook-Air-Timur.local>
2022-05-11 17:16:57 +0300

    create feed/getFeed/getMyFollowing

---
+++ src/controllers/feed.js
@@ -0,0 +1,47 @@
+const { Op } = require("sequelize");
+const { User, Tweet, Follower, sequelize } = require("../sequelize");
+const { getMyRetweets, getMyLikes } = require("./user/globals");
+
+module.exports = {
+  getFeed: async (req, res) => {
+    if (!req.query.userId)
+      return res.status(400).json({ errors: 'userId is required' });
+
+    module.exports.getMyFollowing(req.query.userId).then((response) => {
+      const following = [];
+      response.forEach((el) => following.push(el.id));
+      Promise.all([
+        module.exports.getTweets(following),
+        module.exports.getRetweets(following),
+        module.exports.getLikes(following),
+        getMyLikes(req.query.userId),
+        getMyRetweets(req.query.userId),
+      ]).then((values) => {
+        let retweetSet = new Set();
+        let likeSet = new Set();
+        values[3].map((tweet) => likeSet.add(tweet.tweetId));
+        values[4].map((tweet) => retweetSet.add(tweet.tweetId));
+
+        let tweets = values[0].concat(values[1]).concat(values[2]);
+        const uniqueSet = new Set();
+        tweets = tweets.filter((tweet) => {
+          if (uniqueSet.has(tweet["Tweets.id"])) return false;
+          uniqueSet.add(tweet["Tweets.id"]);
+          return true;
+        });
+        tweets.sort(
+          (a, b) =>
+            new Date(b["Tweets.createdAt"]) - new Date(a["Tweets.createdAt"])
+        );
+        tweets = tweets.map((tweet) => {
+          let deepCopy = { ...tweet };
+          if (retweetSet.has(tweet["Tweets.id"])) deepCopy.selfRetweeted = true;
+          if (likeSet.has(tweet["Tweets.id"])) deepCopy.selfLiked = true;
+          return deepCopy;
+        });
+
+        return res.status(200).json({ tweets: tweets });
+      });
+    });
+  },
+}
