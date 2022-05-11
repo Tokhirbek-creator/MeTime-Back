03f7fa3be0cb8dd2b5a545d018f068a2b786b818
Tokhirbek <karamaov.tohirbek@gmail.com>
2022-05-11 17:06:25 +0300

    user.js function editUser.js

--- src/controllers/user/user.js
+++ src/controllers/user/user.js
@@ -68,4 +68,30 @@ module.exports = {
       return res.status(400).json({ errors });
     }
   },
+  editUser: async (req, res) => {
+    console.log("files", req.files);
+    const avatar = req.files.avatar ? req.files.avatar[0] : null;
+    const cover = req.files.cover ? req.files.cover[0] : null;
+    Promise.all([upload(avatar, "image"), upload(cover, "image")]).then(
+      async (photos) => {
+        const obj = {
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          bio: req.body.bio,
+          location: req.body.location,
+          dob: req.body.dob,
+        };
+        if (photos[0].secure_url) obj.avatar = photos[0].secure_url;
+        if (photos[1].secure_url) obj.cover = photos[1].secure_url;
+        try {
+          const user = await User.update(obj, {
+            where: { id: req.body.userId },
+          });
+          return res.status(200).json({ user: obj });
+        } catch (error) {
+          return res.status(400).json({ errors: error });
+        }
+      }
+    );
+  },
 };
