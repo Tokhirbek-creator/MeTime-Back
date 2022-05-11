bdc290d5291b74d58fc8903494b22fd973925aab
Tokhirbek <karamaov.tohirbek@gmail.com>
2022-05-11 17:37:44 +0300

    follower.js function getDetails

--- src/controllers/follower.js
+++ src/controllers/follower.js
@@ -14,4 +14,34 @@ module.exports = {
       ? res.status(200).json(await Follower.destroy({ where: body }))
       : res.status(200).json(await Follower.create(body));
   },
+  getDetails: async (req, res) => {
+    const values = await Promise.all([
+      module.exports.getFollowers(req.query.id),
+      module.exports.getFollowing(req.query.id),
+      module.exports.getFollowers(req.query.myId),
+      module.exports.getFollowing(req.query.myId),
+    ]);
+    let followers = values[0];
+    let following = values[1];
+    const followersSet = new Set();
+    const followingSet = new Set();
+    values[2].map((item) => followersSet.add(item.id));
+    values[3].map((item) => followingSet.add(item.id));
+    followers = followers.map((item) => {
+      let deepCopy = { ...item };
+      if (followersSet.has(item.id)) deepCopy.isFollower = true;
+      if (followingSet.has(item.id)) deepCopy.isFollowing = true;
+      return deepCopy;
+    });
+    following = following.map((item) => {
+      let deepCopy = { ...item };
+      if (followersSet.has(item.id)) deepCopy.isFollower = true;
+      if (followingSet.has(item.id)) deepCopy.isFollowing = true;
+      return deepCopy;
+    });
+    return res.status(200).json({
+      followers,
+      following,
+    });
+  },
 };
